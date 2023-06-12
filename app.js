19b24bc95392a23945528ab9768cd4e81e93e89b
NaserGharbieh <naserm.gharbieh@gmail.com>
2023-06-12 20:46:13 +0000

    add employees

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 'use stict';
-var EmployeesArray=[]
+var EmployeesArray=[];
+var form=document.getElementById("form"); 
+var secEle = document.getElementById("empinfo");
 
-function Employee(employeeID, fullName, department, level) {
-    this.employeeID = employeeID;
+function Employee(fullName, department, level,imageURL) {
+    
         this.fullName = fullName;
         this.department = department;
         this.level = level;
-        this.imageURL; 
-        this.randomSalary = 0
-        this.netSalary = 0
+        this.imageURL=imageURL; 
+        this.randomSalary = 0;
+        this.netSalary = 0;
+        this.employeeID = generateEmployeeID();
     EmployeesArray.push(this);
 
 }
@@ -31,26 +34,63 @@ Employee.prototype.calculateNetSalary=function(){
     this.netSalary= this.randomSalary-(this.randomSalary*tax);
     
 } 
+function generateEmployeeID(){
+    var empID=1001; 
+    console.log()
+    if(EmployeesArray.length >=1)
+    empID+= EmployeesArray.length;
+   
+return empID;}
 
 Employee.prototype.renderEpmloyee=function(){
+    
+
+
+    let imgEle = document.createElement("img");
+    imgEle.src = this.imageURL;
+    secEle.appendChild(imgEle);
+        document.write(`<p class="info">Employee name : ${this.fullName} , Employee Salary :${this.randomSalary}, Employee Net Salary :${this.netSalary}`);
+    }
+
+    Employee.prototype.printEpmloyee=function(){
     
         document.write(`<p class="info">Employee name : ${this.fullName} , Employee Salary :${this.randomSalary}, Employee Net Salary :${this.netSalary}`);
     }
 
-    
-let ghazi = new Employee(1000, "Ghazi Samer", "Administration", "Senior");
-let lana = new Employee(1001, "Lana Ali", "Finance", "Senior");
-let tamara = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior");
-let safi = new  Employee(1003, "Safi Walid", "Administration", "Mid-Senior");
-let omar = new Employee(1004, "Omar Zaid", "Development", "Senior");
-let rana= new Employee(1005, "Rana Saleh", "Development", "Junior");
-let hadi = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior"); 
+form.addEventListener("submit",addedEployee);
 
-for (let i = 0; i < EmployeesArray.length; i++) {
+function addedEployee(event){
+    event.preventDefault(); 
+    console.log(event);
+
+    let fullname=event.target.fullname.value;
+    let department=event.target.Department.value;
+    let level=event.target.Level.value;
+    let img=event.target.img.value;
+    let newEmployee=new Employee(fullname,department,level,img) ;
+    newEmployee.calculateRandoomSalary();
+    newEmployee.calculateNetSalary(); 
+     newEmployee.renderEpmloyee();
+
+
+
+
+}
+    
+let ghazi = new Employee("Ghazi Samer", "Administration", "Senior");
+let lana = new Employee("Lana Ali", "Finance", "Senior");
+let tamara = new Employee( "Tamara Ayoub", "Marketing", "Senior");
+let safi = new  Employee( "Safi Walid", "Administration", "Mid-Senior");
+let omar = new Employee( "Omar Zaid", "Development", "Senior");
+let rana= new Employee( "Rana Saleh", "Development", "Junior");
+let hadi = new Employee( "Hadi Ahmad", "Finance", "Mid-Senior"); 
+console.log(hadi.employeeID)
+
+/*for (let i = 0; i < EmployeesArray.length; i++) {
     //console.log(EmployeesArray[i]);
     EmployeesArray[i].calculateRandoomSalary();
     EmployeesArray[i].calculateNetSalary();
-    EmployeesArray[i].renderEpmloyee();
-}
+    EmployeesArray[i].printEpmloyee();
+}*/
 
 
